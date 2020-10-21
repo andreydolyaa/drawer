a48feb6c46ee05ed28ab7fb72d16e87bbc67f83b
Andrey Dolya <dolya7kk@gmail.com>
2020-10-21 11:43:26 +0300

    added gallery rendering

--- js/meme-controller.js
+++ js/meme-controller.js
@@ -1,1 +1,33 @@
-'sdfsdfsdfsdf'
+'use strict';
+
+var gCanvas;
+var gCtx;
+
+
+function onInit(){
+    // gCanvas = document.querySelector('#my-canvas');
+    // gCtx = gCanvas.getContext('2d');
+
+    // drawImg();
+    renderGallery();
+}
+
+
+// function drawImg(){
+//     var image = getImgs()
+//     var img = new Image();
+//     img.src = image[0].url;
+//     img.onload = () => {
+//         gCtx.drawImage(img,0,0,gCanvas.width,gCanvas.height);
+//     }
+// }
+
+function renderGallery(){
+    var imgs = getImgs();
+    var strHTMLs = imgs.map(function(img){
+        return `
+        <div id="${img.id}"><img src="${img.url}"></div>
+        `
+    });
+    document.querySelector('.meme-gallery .grid-container').innerHTML = strHTMLs.join('');
+}
---
+++ js/meme-service.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var numOfImgs = 18;
+var gImgs = _createImgs();
+
+
+
+
+
+
+var gMeme = {
+    selectedImgId:5,
+    selectedLineIdx:0,
+
+    lines:[
+        {txt:'HAHAH',size:20,align:'left',color:'red'}
+    ]
+}
+
+
+
+
+
+
+function getImgs(){
+    return gImgs;
+}
+
+
+
+function _createImgs(){
+    var imgs = [];
+    for(var i = 1;i<=numOfImgs;i++){
+        imgs.push(_createImg(i,i))
+    }
+    return imgs;
+}
+
+function _createImg(id,url){
+    return {
+        id:id,
+        url:`img/${url}.jpg`
+    };
+}
+
+console.log(gImgs);
